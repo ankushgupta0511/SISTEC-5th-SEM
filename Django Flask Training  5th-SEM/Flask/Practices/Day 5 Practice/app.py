549812685ffbf34a2c70dd7830cf306f26a644f9
ankushgupta0511 <160836680+ankushgupta0511@users.noreply.github.com>
2024-09-11 15:33:36 +0530

    Add files via upload

--- Django Flask Training  5th-SEM/Flask/Practices/Day 5 Practice/app.py
+++ Django Flask Training  5th-SEM/Flask/Practices/Day 5 Practice/app.py
@@ -6,24 +6,41 @@ app = Flask(__name__)
 
 # DATABASE  CONFIGURATION
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/register'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 # database Model
-class user(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(20), nullable=False)
-    phone_num = db.Column(db.String(12), nullable=False)
-    meg = db.Column(db.String(120), nullable=True)
+    password = db.Column(db.String(12), nullable=False)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/register',methods=['GET','POST'])
 def register():
+    if request.method == 'POST':
+        try:
+            name = request.form.get('name')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            print(name,email,password)
+
+            # adding data in databse
+            print('0')
+            entry = User(name=name,email=email,password=password)
+            print('1')
+            db.session.add(entry)
+            print('2')
+            db.session.commit()
+            print('Data Successfully added!!')
+
+        except Exception as e:
+            print('Errors occure!!')
 
     return render_template('register.html')
 
